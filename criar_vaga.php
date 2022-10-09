fb84f1b2994e09d6ed57b4d343f8958d1ce9e0f3
MarlonVNCS <marlon-vinicius@live.com>
2022-10-09 20:42:22 -0300

    index
    menos bugado

--- criar_vaga.php
+++ criar_vaga.php
@@ -1,5 +1,22 @@
 <?php require_once("validar.php");
 require_once("menu_principal.php");?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Empregue-se</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="js/criar_contas.js"></script>
+    <script src="js/masks.js"></script>
+    <script src="js\jQuery-Mask-Plugin-master\src\jquery.mask.js"></script>
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+
+</head>
+<body class="bg-dark text-light">
         <div class="row">
             <div class="text-center">
                 <h4>Vaga de emprego</h4>
--- index.php
+++ index.php
@@ -2,10 +2,18 @@
 require_once("menu_principal.php");
 ?>
     
-    <div class="">
-        <div class="d-inline-flex gap-5 justify-content-center" >
+<div class="container py-5">
+    <div class="row" >
+
+        <div class="col-lg-3">
+            
 
             <div class="dropdown-menu position-static d-grid gap-1 p-2 rounded-3 mx-0 shadow w-220px">
+
+                <h1 class="h2">Filtros</h1>
+
+                <li><hr class="dropdown-divider"></li>
+
                 <label >Escolaridade</label>
                 <label class="list-group-item d-flex gap-2">
                 <input class="form-check-input flex-shrink-0" type="checkbox" value="" checked="">
@@ -30,6 +38,7 @@ require_once("menu_principal.php");
                 </label>
 
                 <li><hr class="dropdown-divider"></li>
+
                 <label >Cidade</label>
                         <?php  
                             // abre a conexao com o banco de dados
@@ -59,6 +68,20 @@ require_once("menu_principal.php");
                 </label>
             </div>
 
+        </div>
+
+        <div class="col-lg-9">
+            
+            <div class="dropdown d-flex flex-row-reverse">
+                <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Ordenar por 
+                </button>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">A-Z</a></li>
+                    <li><a class="dropdown-item" href="#">Idade</a></li>
+                </ul>
+            </div>
+
             <div class="dropdown-menu position-static d-grid gap-1 p-2 rounded-3 mx-0 shadow w-220px">
                 <?php 
                     $conn = mysqli_connect("127.0.0.1", "root", "", "empregue_se"); // abre a conexão com o banco de dados
@@ -81,30 +104,26 @@ require_once("menu_principal.php");
                                 $disponiveis=$quantidade-$status;
                                 
                                 echo("<a href='ver_vaga.php?id_vaga=$vaga[id]' class='btn btn-primary my-2'>
-                                 Vaga: $nome // Vagas disponiveis: $disponiveis </a>");
+                                    Vaga: $nome // Vagas disponiveis: $disponiveis </a>");
                                 
                                 
                             }
                         }
                     }
                 ?>
-                     
-                <div class="dropdown">
-                        <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Ordenar por 
-                        </button>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">A-Z</a></li>
-                            <li><a class="dropdown-item" href="#">Idade</a></li>
-                        </ul>
-                    </div>
+                        
+                
 
             </div>
-            
-            
+
         </div>
-        <div>
-            
-        </div>
-    
+
+
+        
+        
+    </div>
+    <div>
+        
+    </div>
+
 </div>
