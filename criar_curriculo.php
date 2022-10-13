33553f8733f03ec40708022eb80f83830cc0593d
Mauricio570 <112562968+Mauricio570@users.noreply.github.com>
2022-10-13 11:22:46 -0300

    Add files via upload

--- criar_curriculo.php
+++ criar_curriculo.php
@@ -30,86 +30,93 @@ if($conn){
 
 
 ?>
-        <div class="row">
+        <div class="row" style="margin: 2%;">
             <div class="text-center">
                 <h4>Currículo</h4>
             </div>
             <form method="POST" class="needs-validation">
                 <div class="mb-3">
                     <label for="nome" class="form-label">Nome</label>
-                    <input class="form-control" type="text" value="<?php echo($nome);?>">
+                    <input class="form-control" type="text" name='nome1'  value="<?php echo($nome);?>">
                     
                 </div>
                 <div class="row">
                     <div class="col">
                     <label form="idade" class="form-label">Nascimento</label>
-                      <input class="form-control" type="text" value="<?php echo($nasc);?>">
+                      <input class="form-control" type="date" name='nasc1' value="<?php echo($nasc);?>">
                     </div>
                     <div class="col">
                         <label form="sexo" class="form-label">Sexo</label>
-                        <input class="form-control" type="text" value="<?php echo($sexo);?>">
+                        <input class="form-control" type="text" name='sexo1' value="<?php echo($sexo);?>">
                         <!-- placeholder="Last name" aria-label="Last name"-->
                     </div>
                 </div>
 
                 <div class="mb-3">
                     <label for="area_atuacao" class="form-label">Área de atuação</label>
-                    <textarea class="form-control" id="area_atuacao" rows="4" placeholder="<?php echo($area);?>"></textarea>
+                    <input class="form-control" name="area1" rows="4" value="<?php echo($area);?>"></input>
                 </div>
 
                 <div class="mb-3">
                     <label for="formacao_academica" class="form-label">Formação acadêmica</label>
-                    <textarea class="form-control" id="formacao_academica" rows="4" placeholder="Dados do currículo"></textarea>
+                    <input class="form-control" name="formacao_academica" rows="4" value="Dados do currículo"></input>
                 </div>
 
                 <div class="mb-3">
                     <label for="experiencia" class="form-label">Experiência</label>
-                    <textarea class="form-control" id="experiencia" rows="4" placeholder="<?php echo($expe);?>"></textarea>
+                    <input class="form-control" name="expe1" rows="4" value="<?php echo($expe);?>"></input>
                 </div>
 
                 <div class="row">
                     <div class="col">
                         <label form="email" class="form-label">Email</label>
-                        <input class="form-control" type="text" value="<?php echo($email);?>">
+                        <input class="form-control" type="text" name='email1' value="<?php echo($email);?>">
                     </div>
                     <div class="col">
                         <label form="numero_telefone" class="form-label">Número de telefone</label>
-                            <input class="form-control" type="text" value="<?php echo($tel);?>">
+                            <input class="form-control" type="text" name='tel1' value="<?php echo($tel);?>">
                         </div>
                     </div>
                     
                     
                     <div class="row"> 
-                        <div class="d-grid gap-2 col-3 mx-auto">
+                        <div class="d-grid gap-2 col-3 mx-auto" style="margin: 15px;">
                             <button class="btn btn-light btn-lg btn-block" type="submit" name="atualiza">Atualizar</button>
                         </div>  
-                    <div class="d-grid gap-2 col-3 mx-auto">
+                    <div class="d-grid gap-2 col-3 mx-auto" style="margin: 15px;">
                         <a href="conta_cliente.php" class="btn btn-light btn-lg btn-block" type="submit">Voltar</a>
                     </div>
                 </div>
                 <?php
                     if(isset($_POST["atualiza"])){
-                        $conn = mysqli_connect("localhost", "root", "", "empregue_se");
+                        $nome1=$_POST['nome1'];
+                        $nasc1=$_POST['nasc1'];
+                        $tel1=$_POST['tel1'];
+                        $email1=$_POST['email1'];
+                        $area1=$_POST['area1'];
+                        $expe1=$_POST['expe1'];
+                        $sexo1=$_POST['sexo1'];
                         if($conn){
-                            //if($sexo == 'Masculino' || $sexo == 'masculino' || $sexo == 'Feminino' || $sexo == 'feminino'){
-                                /*if($sexo == 'Masculino' || $sexo == 'masculino'){
-                                    $sexo = "m";
-                                }else if($sexo == 'Feminino' || $sexo == 'feminino'){
-                                    $sexo = "f";
+                            if($sexo1 == 'Masculino' || $sexo1 == 'masculino' || $sexo1 == 'Feminino' || $sexo1 == 'feminino'){
+                                if($sexo1 == 'Masculino' || $sexo1 == 'masculino'){
+                                    $sexo1 = "m";
+                                }else if($sexo1 == 'Feminino' || $sexo1 == 'feminino'){
+                                    $sexo1 = "f";
                                 }else{
                                     echo("erro no cadastro");
                                     header(location: "conta_cliente");
-                                }*/
-                               //$sql = "UPDATE cliente SET nome = '$nome', cpf = '$cpf', nascimento = '$nasc', telefone='$tel',endereco ='$end',email='$email', areaDeAtuacao = '$area', experiencia = '$expe', sexo='$sexo' WHERE id = '$id_cli'";
-                                $sql = "UPDATE cliente SET nome = '$nome' WHERE id = '$id_cli'";
+                                }
+                              //echo $sql = "UPDATE cliente SET nome = '$nome1', cpf = '$cpf1', nascimento = '$nasc1', telefone='$tel',endereco ='$end',email='$email', areaDeAtuacao = '$area', experiencia = '$expe', sexo='$sexo' WHERE id = '$id_cli'";
+                                $sql = "UPDATE cliente SET nome = '$nome1', nascimento = '$nasc1', telefone='$tel1',email='$email1', areaDeAtuacao = '$area1', experiencia = '$expe1', sexo = '$sexo1' WHERE id = '$id_cli'";
+                            
                                 if (mysqli_query($conn, $sql)){
                                     echo ("
                                     <script>
-                                    alert('Curruculo criado com sucesso');
+                                    alert('Curriculo criado com sucesso');
                                     location.href = 'conta_cliente.php';
                                     </script>");
                                 }else{echo("Tudo errado");}
-                            //}
+                            }
                         }
                     
                     }
