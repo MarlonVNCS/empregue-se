8f5888a50988199374e4a6575d61c011145e7953
Mauricio570 <112562968+Mauricio570@users.noreply.github.com>
2022-10-19 11:12:51 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,47 +1,54 @@
 <?php
 require_once("menu_principal.php");
-session_start();
+
 ?>
-    
+
 <div class="container py-5">
-    <div class="row" >
+    <div class="row">
 
         <div class="col-lg-3">
-            
+
 
             <div class="dropdown-menu position-static d-grid gap-1 p-2 rounded-3 mx-0 shadow w-220px">
 
                 <h1 class="h2">Filtros</h1>
 
-                <li><hr class="dropdown-divider"></li>
+                <li>
+                    <hr class="dropdown-divider">
+                </li>
 
-                <label >Escolaridade</label>
+                <label>Escolaridade</label>
                 <label class="list-group-item d-flex gap-2">
-                <input class="form-check-input flex-shrink-0" type="checkbox" value="" checked="">
-                <span><label style="vertical-align: inherit;"><label style="vertical-align: inherit;">
-                Ensino fundamental
-                    </label></label><small class="d-block text-muted"><label style="vertical-align: inherit;"></label></small>
-                </span>
+                    <input class="form-check-input flex-shrink-0" type="checkbox" value="" checked="">
+                    <span><label style="vertical-align: inherit;"><label style="vertical-align: inherit;">
+                                Ensino fundamental
+                            </label></label><small class="d-block text-muted"><label
+                                style="vertical-align: inherit;"></label></small>
+                    </span>
                 </label>
                 <label class="list-group-item d-flex gap-2">
-                <input class="form-check-input flex-shrink-0" type="checkbox" value="">
-                <span><label style="vertical-align: inherit;"><label style="vertical-align: inherit;">
-                    Ensino médio
-                    </label></label><small class="d-block text-muted"><label style="vertical-align: inherit;"></label></small>
-                </span>
+                    <input class="form-check-input flex-shrink-0" type="checkbox" value="">
+                    <span><label style="vertical-align: inherit;"><label style="vertical-align: inherit;">
+                                Ensino médio
+                            </label></label><small class="d-block text-muted"><label
+                                style="vertical-align: inherit;"></label></small>
+                    </span>
                 </label>
                 <label class="list-group-item d-flex gap-2">
-                <input class="form-check-input flex-shrink-0" type="checkbox" value="">
-                <span><label style="vertical-align: inherit;"><label style="vertical-align: inherit;">
-                Ensino superior
-                    </label></label><small class="d-block text-muted"><label style="vertical-align: inherit;"></label></small>
-                </span>
+                    <input class="form-check-input flex-shrink-0" type="checkbox" value="">
+                    <span><label style="vertical-align: inherit;"><label style="vertical-align: inherit;">
+                                Ensino superior
+                            </label></label><small class="d-block text-muted"><label
+                                style="vertical-align: inherit;"></label></small>
+                    </span>
                 </label>
 
-                <li><hr class="dropdown-divider"></li>
+                <li>
+                    <hr class="dropdown-divider">
+                </li>
 
-                <label >Cidade</label>
-                        <?php  
+                <label>Cidade</label>
+                <?php  
                             // abre a conexao com o banco de dados
                             $conn = mysqli_connect("localhost", "root", "", "empregue_se");
 
@@ -65,17 +72,18 @@ session_start();
                             }
                         ?>
 
-                    
+
                 </label>
             </div>
 
         </div>
 
         <div class="col-lg-9">
-            
+
             <div class="dropdown d-flex flex-row-reverse">
-                <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Ordenar por 
+                <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown"
+                    aria-expanded="false">
+                    Ordenar por
                 </button>
                 <ul class="dropdown-menu">
                     <li><a class="dropdown-item" href="#">A-Z</a></li>
@@ -92,10 +100,16 @@ session_start();
                             
                     }
                     else{
-                        if($conn) {
-                            $sql = "SELECT * FROM vaga ORDER BY nome ASC";
-                            $resultado = mysqli_query($conn, $sql);
-                            
+                        if(isset($_SESSION['tipo'])){
+                            if($_SESSION['tipo'] == 'empresa'){    
+                                $id_empresa=$_SESSION["login"];
+                                $sql = "SELECT * FROM vaga WHERE id_empresa=$id_empresa";
+                                $resultado = mysqli_query($conn, $sql);
+                            }else{
+                                $sql = "SELECT * FROM vaga ORDER BY nome ASC";
+                                $resultado = mysqli_query($conn, $sql);
+                            }
+                                    
                             while ($vaga =  mysqli_fetch_array($resultado)) {
                                 $id = $vaga["id"];
                                 $nome = $vaga["nome"];
@@ -103,26 +117,43 @@ session_start();
                                 $status = $vaga["status"];
                                 $disponiveis=$quantidade-$status;
                                 echo("<a href='ver_vaga.php?id=$vaga[id]' class='btn btn-primary my-2'>
-                                    Vaga: $nome // Vagas disponiveis: $disponiveis </a>");
-                                
-                                
+                                Vaga: $nome // Vagas disponiveis: $disponiveis</a>");
+                                        
+                                        
+                            }
+                        }else{
+                            $sql = "SELECT * FROM vaga ORDER BY nome ASC";
+                            $resultado = mysqli_query($conn, $sql);
+                                    
+                            while ($vaga =  mysqli_fetch_array($resultado)) {
+                                $id = $vaga["id"];
+                                $nome = $vaga["nome"];
+                                $quantidade = $vaga["quantidade"];
+                                $status = $vaga["status"];
+                                $disponiveis=$quantidade-$status;
+                                echo("<a href='ver_vaga.php?id=$vaga[id]' class='btn btn-primary my-2'>
+                                Vaga: $nome // Vagas disponiveis: $disponiveis</a>");
+
                             }
                         }
                     }
+
+
+
                 ?>
-                        
-                
+
+
 
             </div>
 
         </div>
 
 
-        
-        
+
+
     </div>
     <div>
-        
+
     </div>
 
 </div>
