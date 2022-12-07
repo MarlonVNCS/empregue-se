cd33217fdb9ed1e3f69a53d607bbee3249647363
LucasScheffer18 <101944231+LucasScheffer18@users.noreply.github.com>
2022-12-07 18:55:09 -0300

    Add files via upload

--- cadastro_cliente.php
+++ cadastro_cliente.php
@@ -1,71 +1,73 @@
 <?php require_once("menu_login.php")?>
+<link rel="stylesheet" href="estilo.css">
+
 <div class="row">
-    <div class="text-center">
-        <h4>Criar conta</h4>
-    </div>
-    <form method="POST" class="needs-validation">
-        <div class="mb-3">
-            <label for="nome" class="form-label">Nome</label>
-            <input type="text" class="form-control" id="nome" name="nome">
+    <form method="POST">
+        <div class="box">
+            <div class="form">
+                <h2>Criar conta</h2>
+                <div class="mb-3">
+                    <label for="nome" class="form-label">Nome</label>
+                    <input type="text" class="form-control" id="nome" name="nome">
+                </div>
+                <div class="mb-3">
+                    <label for="nascimento">Data de nascimento</label>
+                    <input type="date" class="form-control" id="nascimento" name="nascimento">
+                </div>
+                <div class="mb-3">
+                    <label for="telefone">Telefone</label>
+                    <input type="text" class="form-control" id="telefone" name="telefone" placeholder="(XX) XXXXX-XXXX">
+                </div>
+                <div class="mb-3">
+                    <label for="cpf">CPF</label>
+                    <input type="text" class="form-control" id="cpf" name="cpf" placeholder="000.000.000-00">
+                </div>
+                <div class="mb-3">
+                    <label for="sexo">Gênero</label>
+                    <select class="form-select" name="sexo">
+                        <option value="m">Masculino</option>
+                        <option value="f">Feminino</option>
+                    </select>
+                </div>
+                <div class="mb-3">
+                    <label for="email">Email</label>
+                    <input type="email" class="form-control" name="email" placeholder="email@exemplo.com">
+                </div>
+                <div class="mb-3">
+                    <label for="formacao">Formação Acadêmica</label>
+                    <select class="form-select" name="formacao">
+                        <?php
+                                $conn = mysqli_connect("localhost", "root", "", "empregue_se");
 
-        </div>
-        <div class="mb-3">
-            <label for="nascimento">Data de nascimento</label>
-            <input type="date" class="form-control" id="nascimento" name="nascimento">
-        </div>
-        <div class="mb-3">
-            <label for="telefone">Telefone</label>
-            <input type="text" class="form-control" id="telefone" name="telefone" placeholder="(XX) XXXXX-XXXX">
-        </div>
-        <div class="mb-3">
-            <label for="cpf">CPF</label>
-            <input type="text" class="form-control" id="cpf" name="cpf" placeholder="000.000.000-00">
-        </div>
-        <div class="mb-3">
-            <label for="sexo">Gênero</label>
-            <select class="form-select" name="sexo">
-                <option value="m">Masculino</option>
-                <option value="f">Feminino</option>
-            </select>
-        </div>
-        <div class="mb-3">
-            <label for="email">Email</label>
-            <input type="email" class="form-control" name="email" placeholder="email@exemplo.com">
-        </div>
-        <div class="mb-3">
-            <label for="formacao">Formação Acadêmica</label>
-            <select class="form-select" name="formacao">
-                <?php
-                        $conn = mysqli_connect("localhost", "root", "", "empregue_se");
+                                if ($conn){
+                                    $sql = "SELECT * FROM formacaoacademica ORDER BY id ASC";
+                                    $registros = mysqli_query($conn, $sql);
 
-                        if ($conn){
-                            $sql = "SELECT * FROM formacaoacademica ORDER BY id ASC";
-                            $registros = mysqli_query($conn, $sql);
+                                    if(mysqli_num_rows($registros) >0 ){
 
-                            if(mysqli_num_rows($registros) >0 ){
-
-                                while ($registro =  mysqli_fetch_array($registros)) {
-                                echo("<option value='$registro[id]'>$registro[descricao]</option>");
+                                        while ($registro =  mysqli_fetch_array($registros)) {
+                                        echo("<option value='$registro[id]'>$registro[descricao]</option>");
+                                        }
+                                    }
                                 }
-                            }
-                        }
-                    ?>
-            </select>
-        </div>
-        <div class="mb-3">
-            <label for="senha">Senha</label>
-            <input type="password" class="form-control" name="senha">
-        </div>
-        <div class="d-grid gap-2 col-3 mx-auto">
-            <button class="btn btn-light btn-lg btn-block" type="submit" name="cria">Criar</button>
-        </div>
-    </form>
-    <div class="text-danger">
-        <!-- Essa classe só ta para destacar o texto enquanto n arrumamos o botão-->
-        <a href="login.php" style="font-size:110%;" class="text-info text-decoration-none">Voltar</a>
-        <div id="contas"></div>
+                            ?>
+                    </select>
+                </div>
+                <div class="mb-3">
+                    <label for="senha">Senha</label>
+                    <input type="password" class="form-control" name="senha">
+                </div>
+                <div class="row" style="margin-top: 20px;">
+                    <div class="d-grid gap-2 col-3 mx-auto">
+                        <button class="btn btn-light btn-lg btn-block" type="submit" name="cria">Criar</button>
+                    </div>
+                    <div class="d-grid gap-2 col-3 mx-auto">
+                        <a href="login.php" class="btn btn-light btn-lg btn-block" type="submit">Voltar</a>
+                    </div>
+                </div>
+            </div>
+        </form>
     </div>
-</div>
 </div>
 <?php
     if(isset($_POST["cria"])){
@@ -78,12 +80,12 @@
             $id_formacao =mysqli_real_escape_string($conn, $_POST["formacao"]);
             $sexo=mysqli_real_escape_string($conn, $_POST["sexo"]);
 
-            $salt="DsVg$3";
-                $junta = $salt.$senha;
+            $sal="DsVg$3";
+                $junta=$sal.$senha;
                 $cripto_senha=hash("sha512",$junta);
 
             if($conn){
-                $sql = "INSERT INTO cliente(nome,cpf,nascimento, telefone, email,senha,sexo,id_formacaoAcademica) VALUES ('$nome','$cpf', '$nasc','$telefone','$email','$$cripto_senha','$sexo',$id_formacao)";
+                $sql = "INSERT INTO cliente(nome,cpf,nascimento, telefone, email,senha,sexo,id_formacaoAcademica) VALUES ('$nome','$cpf', '$nasc','$telefone','$email','$cripto_senha','$sexo',$id_formacao)";
                 if(mysqli_query($conn, $sql)){
                     echo ("
                     <script>
--- cadastro_empresa.php
+++ cadastro_empresa.php
@@ -1,40 +1,42 @@
 <?php require_once("menu_login.php")?>
+<link rel="stylesheet" href="estilo.css">
+
 <div class="row">
-    <div class="text-center">
-        <h4>Cadastro empresa</h4>
-    </div>
-    <form method="POST" class="needs-validation">
-        <div class="mb-3">
-            <label for="nome" class="form-label">Nome</label>
-            <input type="text" class="form-control" name="nome">
-
-        </div>
-        <div class="mb-3">
-            <label for="Endereço" class="form-label">Endereço</label>
-            <input type="text" class="form-control" name="Endereço">
-        </div>
-
-        <div class="mb-3">
-            <label for="cnpj">CNPJ</label>
-            <input type="text" class="form-control" id="cnpj" name="cnpj" placeholder="00.000.000/0000-00">
-        </div>
-        <div class="mb-3">
-            <label for="email">Email</label>
-            <input type="email" class="form-control" name="email" placeholder="email@exemplo.com">
-        </div>
-        <div class="mb-3">
-            <label for="senha">Senha</label>
-            <input type="password" class="form-control" name="senha">
-        </div>
-        <div class="d-grid gap-2 col-3 mx-auto">
-            <button class="btn btn-light btn-lg btn-block" type="submit" name='cria'>Criar</button>
+    <form method="POST">
+        <div class="box1">
+            <div class="form">
+                <h2>Cadastro empresa</h2>
+                <div class="mb-3">
+                    <label for="nome" class="form-label">Nome</label>
+                    <input type="text" class="form-control" name="nome">
+                </div>
+                <div class="mb-3">
+                    <label for="Endereço" class="form-label">Endereço</label>
+                    <input type="text" class="form-control" name="Endereço">
+                </div>
+                <div class="mb-3">
+                    <label for="cnpj">CNPJ</label>
+                    <input type="text" class="form-control" id="cnpj" name="cnpj" placeholder="00.000.000/0000-00">
+                </div>
+                <div class="mb-3">
+                    <label for="email">Email</label>
+                    <input type="email" class="form-control" name="email" placeholder="email@exemplo.com">
+                </div>
+                <div class="mb-3">
+                    <label for="senha">Senha</label>
+                    <input type="password" class="form-control" name="senha">
+                </div>
+                <div class="row" style="margin-top: 20px;">
+                    <div class="d-grid gap-2 col-3 mx-auto">
+                        <button class="btn btn-light btn-lg btn-block" type="submit" name="cria">Criar</button>
+                    </div>
+                    <div class="d-grid gap-2 col-3 mx-auto">
+                        <a href="login.php" class="btn btn-light btn-lg btn-block" type="submit">Voltar</a>
+                    </div>
+                </div>
+            </div>
         </div>
     </form>
-    <div class="text-warning">
-        <a href="login.php" style="font-size:110%;" class="text-info text-decoration-none">Voltar</a>
-        <div id="contas"></div>
-    </div>
-</div>
 </div>
 <?php
     $conn = mysqli_connect("localhost", "root", "", "empregue_se");
@@ -46,7 +48,7 @@
             $endereco = mysqli_real_escape_string($conn, $_POST["endereco"]);
 
             $salt="DsVg$3";
-                $junta = $salt.$senha;
+                $junta=$salt.$senha;
                 $cripto_senha=hash("sha512",$junta);
 
             if($conn){
